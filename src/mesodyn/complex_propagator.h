37cfb19767a92c9ed6ee84aff5daf813892044e9
Daniël Emmery <daniel.emmery@protonmail.com>
2026-02-12 14:47:05 +0100

    Style updates

--- src/mesodyn/complex_propagator.h
+++ src/mesodyn/complex_propagator.h
@@ -3,19 +3,15 @@
 
 #include "../namics.h"
 #include "../lattice.h"
+#include "stl_typedef.h"
 
-// Complex Scheutjens-Fleer lattice propagation using paired Real arrays.
-// Reads lattice geometry (lambda weights, strides) from Lat via Side().
 class ComplexPropagator {
 public:
 	ComplexPropagator(Lattice* lat);
 	~ComplexPropagator();
 
-	// G1 = exp(-(W_R + i*W_I))
 	void compute_boltzmann(Real* W_R, Real* W_I, Real* G1_R, Real* G1_I);
 
-	// One propagation step: dst = G1 * Side(src)
-	// src arrays are modified by set_bounds (boundary fill).
 	void propagate_step(Real* dst_R, Real* dst_I,
 	                    Real* src_R, Real* src_I,
 	                    Real* G1_R, Real* G1_I);
@@ -23,8 +19,8 @@ public:
 private:
 	Lattice* lat;
 	int M;
-	Real* buf_R;
-	Real* buf_I;
+	stl::device_vector<Real> buf_R;
+	stl::device_vector<Real> buf_I;
 };
 
 #endif
